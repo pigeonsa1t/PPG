501d8909ce08deaeca7b23c575280e10c9ae5e18
pigeonsa1t <bowen_samantha@yahoo.com>
2022-03-20 17:11:03 -0400

    updated after meeting with tutor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,26 @@
+var lowerStr = "abcdefghijklmnopqrstuvwxyz"
+var lowerArr = lowerStr.split("");
+var upperStr = lowerStr.toUpperCase("");
+var upperArr = upperStr.split("");
+var numbersArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialArr = ["&", "|", "^"];
 // Assignment code here
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+  console.log(generateBtn);
+
+function generatePassword() {
+  var passLength = parseInt(window.prompt("How long do you want to make your password?"));
+  var lowerCase = confirm("Would you like to use lowercase letters?")
+  var upperCase = confirm("Would you like to use uppercase letters?")
+  var numbers = confirm("Would you like to use numbers?")
+  var specialCharacters = confirm("Would you like to use special characters?")
+    console.group(lowerCase, upperCase, numbers, specialCharacters)
+    if (specialCharacters)
+  return "Hello :)";
+}
 
 // Write password to the #password input
 function writePassword() {
